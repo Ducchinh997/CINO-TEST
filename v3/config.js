3357a5eac415cf2d8b06ff49fc2bb858f9877e74
Ducchinh997 <129574954+Ducchinh997@users.noreply.github.com>
2025-08-26 01:19:54 +0700

    Add files via upload

---
+++ v3/config.js
@@ -0,0 +1,23 @@
+const weddingConfig = {
+  date: "2025-09-02",
+  couple: {
+    groom: { name: "Đức Chính", img: "groom.jpg", desc: "Chú rể hiền lành, vui tính,ham ăn." },
+    bride: { name: "Minh Nguyệt", img: "bride.jpg", desc: "Cô dâu dịu dàng, xinh đẹp,lười làm." }
+  },
+  timeline: [
+    { year: "2016", text: "Lần đầu gặp gỡ" },
+    { year: "2018", text: "Hẹn hò lần đầu tiên" },
+    { year: "2019", text: "Lời cầu hôn" },
+    { year: "2020", text: "Ngày cưới hạnh phúc" }
+  ],
+  events: [
+    { title: "Lễ cưới nhà trai", time: "11:00, 15/06/2025", place: "Nam Định", map: "#", ics: "#" },
+    { title: "Lễ cưới nhà gái", time: "08:30, 15/06/2025", place: "Hà Nội",   map: "#", ics: "#" }
+  ],
+  album: ["photo (1).jpg","photo (2).jpg","photo (3).jpg","photo (4).jpg","photo (5).jpg","photo (6).jpg","photo (7).jpg","photo (8).jpg","photo (9).jpg","photo (10).jpg","photo (11).jpg","photo (12).jpg","photo (13).jpg","photo (14).jpg","photo (15).jpg"],
+  video: "https://www.youtube.com/embed/hCTkRHEIgXM",
+  bank: {
+    name1: "Vietcombank", account1: "0821000107509", owner1: "NGUYEN DUC CHINH", qr1: "qr1.png",
+    name2: "Vietcombank", account2: "0821000107509", owner2: "NGUYEN DUC CHINH", qr2: "qr2.png"
+  }
+};
